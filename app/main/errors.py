9b892041867ed0f1d703c79de51f2ef355775456
harry <harryx520@qq.com>
2016-04-16 20:41:05 +0800

    add user permissions

--- app/main/errors.py
+++ app/main/errors.py
@@ -5,3 +5,7 @@ from . import main
 @main.app_errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+@main.app_errorhandler(403)
+def forbidden(e):
+    return render_template('403.html'), 403
