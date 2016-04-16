ca5e0c1cea0943d1876fc49f43527dd71501dc21
harry <harryx520@qq.com>
2016-04-16 10:21:18 +0800

    add login logout change_password change_email reset_password

--- config.py
+++ config.py
@@ -4,6 +4,14 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Harry]'
+    FLASKY_MAIL_SENDER = 'harryx520@qq.com'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
     @staticmethod
     def init_app(app):
